c535296d3d20f6dafd4508c14be5b89650a4db1a
André Silva <andresilva.webdev@gmail.com>
2026-08-30 15:38:52 +0000

    feat: atualizar configuração de variáveis de ambiente e sincronização de presenças

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // ou '0.0.0.0'
+    host: true, // Necessário para o Codespaces expor a porta corretamente
     port: 5173,
-    strictPort: true,
-  }
+  },
 })
